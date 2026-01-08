d850a2bd3269adfea3c8cebec3141f0e5f874e02
Mustafa Wissam sabah – Front-end Developer <52778859+MURoyale@users.noreply.github.com>
2026-01-08 18:21:48 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -82,11 +82,11 @@ export default function Navbar() {
       href: "/purchasing",
       current: true,
     },
-    {
-      name: "الفواتير",
-      href: "/order",
-      current: true,
-    },
+    // {
+    //   name: "الفواتير",
+    //   href: "/order",
+    //   current: true,
+    // },
     {
       name: "المنتجات",
       href: "/product",
